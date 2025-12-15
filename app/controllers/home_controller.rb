f912b2cf6cbf56f17cb8f7b32ad9edb6f8eb07db
Allan J. Souza <allanjsouza@gmail.com>
2025-12-15 16:27:55 -0300

    feat: setup public (skip auth) home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
+    @popular_courses = Course.all.limit(3)
+    @top_rated_courses = Course.all.limit(3)
+    @most_recent_courses = Course.all.limit(3).order(created_at: :desc)
   end
 end
