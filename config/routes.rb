98a8de01f2900c2845c19bee33071f0a717da45f
Allan J. Souza <allanjsouza@gmail.com>
2025-12-15 11:12:34 -0300

    feat: add course resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-
+  
+  resources :courses
+  
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
