641c7d9003bfc236f7488bdf9b56a038c79976cd
Allan J. Souza <allanjsouza@gmail.com>
2025-12-15 11:52:00 -0300

    feat: add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
+
+  devise_for :users
+
   resources :courses
-  
+
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
