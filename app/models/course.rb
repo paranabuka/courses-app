dfed084f09eba22b48015e214fa20b8fb5b42c9b
Allan J. Souza <allanjsouza@gmail.com>
2025-12-29 17:35:16 -0300

    feat: add lessons to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   validates :description, presence: true, length: { minimum: 5 }
 
   belongs_to :user
+  has_many :lessons, dependent: :destroy
 
   extend FriendlyId
   friendly_id :title, use: :slugged
@@ -10,27 +11,27 @@ class Course < ApplicationRecord
   has_rich_text :description
 
   include PublicActivity::Model
-  tracked owner: ->(controller, model) { controller.current_user }
+  tracked owner: ->(controller, _model) { controller.current_user }
 
   def to_s
     title
   end
 
-  LANGUAGES = %w[English Portuguese Spanish French German]
+  LANGUAGES = %w[English Portuguese Spanish French German].freeze
   def self.languages
     LANGUAGES.map { |lang| [lang, lang] }
   end
 
-  LEVELS = %w[Beginner Intermediate Advanced]
+  LEVELS = %w[Beginner Intermediate Advanced].freeze
   def self.levels
     LEVELS.map { |level| [level, level] }
   end
 
-  def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(_auth_object = nil)
     %w[title short_description language level price]
   end
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_associations(_auth_object = nil)
     %w[user]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :courses
+  resources :lessons
 
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
