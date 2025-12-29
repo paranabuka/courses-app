79ce7c42a05de2206bf278f0059641b68831d592
Allan J. Souza <allanjsouza@gmail.com>
2025-12-29 14:07:01 -0300

    feat: add authorization on courses (pundit gem)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,20 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   include PublicActivity::StoreController
+  include Pundit::Authorization
 
   before_action :set_global_variables, if: :user_signed_in?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(root_path)
   end
 end
