8b987f1c4515c5b7b8c9a654328eccba3d60e308
Allan J. Souza <allanjsouza@gmail.com>
2025-12-29 21:42:26 -0300

    feat: add lesson directly from course

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,13 @@ class Lesson < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  has_rich_text :content
+
+  include PublicActivity::Model
+  tracked owner: ->(controller, _model) { controller.current_user }
+
+  def to_s
+    title
+  end
 end
