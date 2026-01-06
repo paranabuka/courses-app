881dac7cf78bfabedb809a0e0192a6b56b12ab00
Allan J. Souza <allanjsouza@gmail.com>
2026-01-06 17:20:15 -0300

    feat: handle course/lesson/user destruction

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   belongs_to :user, counter_cache: true
 
   has_many :lessons, dependent: :destroy
-  has_many :enrollments
+  has_many :enrollments, dependent: :restrict_with_error
   has_many :user_lessons, through: :lessons
 
   validates :title, :short_description, :language, :level, :price, presence: true
