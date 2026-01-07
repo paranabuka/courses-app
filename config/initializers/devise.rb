5ccc282cd0ff581759feedd240796226bd760ecf
Allan J. Souza <allanjsouza@gmail.com>
2026-01-07 19:19:12 -0300

    fix: remove deprecation warning

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV['DEVISE_MAILER_SENDER'] || ApplicationMailer.default_params[:from]
+  config.mailer_sender = ENV['DEVISE_MAILER_SENDER'] || 'support@courses.paranabuka.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
