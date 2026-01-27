b491875cf0a930b5b3ea74e7442e03cd5962bf59
Allan J. Souza <allanjsouza@gmail.com>
2026-01-27 17:59:27 -0300

    feat: add google omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :courses, dependent: :nullify
   has_many :enrollments, dependent: :nullify
@@ -28,6 +29,18 @@ class User < ApplicationRecord
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[id email]
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    return user if user
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    User.create(email: data['email'],
+                password: Devise.friendly_token[0, 20],
+                confirmed_at: Time.now) # Autommatically confirm omniauth users
   end
 
   def online?
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
   
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   
   resources :users, only: [:index, :show, :edit, :update]
 
