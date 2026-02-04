df4eed2b4507136b8a74ecdda2940acf6e2d160c
Allan J. Souza <allanjsouza@gmail.com>
2026-02-04 14:31:01 -0300

    fix: mailers preview since there is no need to deliver

--- test/mailers/previews/enrollment_mailer_preview.rb
+++ test/mailers/previews/enrollment_mailer_preview.rb
@@ -1,10 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/enrollment_mailer
 class EnrollmentMailerPreview < ActionMailer::Preview
   def new_enrollment
-    EnrollmentMailer.new_enrollment(Enrollment.last).deliver_now
+    EnrollmentMailer.new_enrollment(Enrollment.last)
   end
 
   def new_student
-    EnrollmentMailer.new_student(Enrollment.last).deliver_now
+    EnrollmentMailer.new_student(Enrollment.last)
   end
 end
